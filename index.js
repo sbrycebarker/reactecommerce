d2a0669b27626b185248ea414fdaf89e75bd303d
sbrycebarker <sbrycesti@gmail.com>
2020-07-18 02:11:47 -0600

    test

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const express = require('express');
-const session = requre('sessoin');
-const DB = require('./server/DB');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+//  CONNECT To Database
+connectDB();
+
+console.log("this is a test")
+
+// Init Middleware
+app.use(express.json({extended: false}));
+
+app.get('/', (req, res) => res.send('Well, hello there!'));
+
+app.use('/api/wish', require('./routes/api/wish'));
+app.use('/api/purchase', require('./routes/api/purchase'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+// Serve static assets in production
+
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'))
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(_dirname, 'client, build, index.html'))
+//     })
+// }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export default class navbar extends Component {
   render() {
     return (
-      <div className="navbar">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         { this.props.children }
-        <h1>Welcome</h1>
-      </div>
+        <h1>WELCOME TO MY SITE</h1>
+      </nav>
     )
   }
 }
